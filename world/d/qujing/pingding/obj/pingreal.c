ef5e187ec31c21559d333e9811289f3ec514ec03
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:26:00 +0800

    Remove trailing spaces

--- world/d/qujing/pingding/obj/pingreal.c
+++ world/d/qujing/pingding/obj/pingreal.c
@@ -1,14 +1,14 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 // by snowcat 12/17/1997
 
 #include <ansi.h>
 #include <weapon.h>
- 
+
 inherit F_UNIQUE;
 inherit ITEM;
- 
+
 void create()
 {
   set_name("羊脂玉净瓶",({"jing ping", "jing", "ping", "fabao"}));
@@ -31,7 +31,7 @@ void create()
   set("replace_file", "/d/qujing/pingding/obj/pingfake.c");
   set("is_monitored",1);
   setup();
-} 
+}
 
 void init ()
 {
