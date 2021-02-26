163841517ad95159cf60eb67b21c5164b5cd5825
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:28:54 +0800

    Remove trailing spaces

--- world/d/suburb/fy/fywall/ewall6.c
+++ world/d/suburb/fy/fywall/ewall6.c
@@ -10,7 +10,7 @@ LONG
         );
         set("exits", ([ /* sizeof() == 4 */
   "north" : __DIR__"eupgate",
-  "south"  : __DIR__"ewall7", 
+  "south"  : __DIR__"ewall7",
 ]));
         set("outdoors", "fengyun");
         setup();
