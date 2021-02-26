8f40248e874e776f4e36b33a6eb9d3e7c62c1a97
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:19:36 +0800

    Remove trailing spaces

--- world/cmds/std/drop.c
+++ world/cmds/std/drop.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 // drop.c
 
 inherit F_CLEAN_UP;
@@ -18,14 +18,14 @@ int main(object me, string arg)
 
         if(me->is_busy())
            return notify_fail("你正忙着呢．．．\n");
- 
+
    if(!arg) return notify_fail("你要丢弃什么东西？\n");
 
    if(sscanf(arg, "%d %s", amount, item)==2) {
      if( !objectp(obj = present(item, me)) )
         return notify_fail("你身上没有这样东西。\n");
      if( no_drop=obj->query("no_drop") )
-                        return notify_fail( stringp(no_drop) ? 
+                        return notify_fail( stringp(no_drop) ?
           no_drop : "这样东西不能随意丢弃。\n");
      if( !obj->query_amount() )
         return notify_fail( obj->name() + "不能被分开丢弃。\n");
@@ -59,7 +59,7 @@ int main(object me, string arg)
    if(!objectp(obj = present(arg, me)))
      return notify_fail("你身上没有这样东西。\n");
    if( no_drop=obj->query("no_drop") )
-                return notify_fail( stringp(no_drop) ? 
+                return notify_fail( stringp(no_drop) ?
         no_drop : "这样东西不能随意丢弃。\n");
    return do_drop(me, obj);
 }
@@ -68,7 +68,7 @@ int do_drop(object me, object obj)
 {
    if(me->is_busy())
       return notify_fail("你正忙着呢．．．\n");
-      
+
    if( obj->query("no_drop") )
      return notify_fail("这样东西不能随意丢弃。\n");
 
@@ -78,7 +78,7 @@ int do_drop(object me, object obj)
      if( obj->query_temp("is_living")==1 )
             message_vision("$N将$n从背上放了下来，躺在地上。\n", me, obj);
      else {
-       message_vision( sprintf("$N丢下一%s$n。\n",   
+       message_vision( sprintf("$N丢下一%s$n。\n",
          undefinedp(obj->query_temp("unit"))?
          "个":obj->query_temp("unit")
          ), me, obj );
@@ -104,11 +104,11 @@ int help(object me)
 {
    write(@HELP
 指令格式 : drop <物品名称>
- 
+
 这个指令可以让你丢下你所携带的物品.
- 
+
 HELP
     );
     return 1;
 }
- 
+
