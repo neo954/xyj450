1615ce3558db354e42cf72db2f1797134de81a5e
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:27:09 +0800

    Remove trailing spaces

--- world/d/qujing/wuzhuang/npc/mingyue.c
+++ world/d/qujing/wuzhuang/npc/mingyue.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 //mingyue.c
 
 inherit NPC;
@@ -27,7 +27,7 @@ void create()
 }
 
 void init()
-{       
+{
    object ob=this_player();
    object me=this_object();
 
