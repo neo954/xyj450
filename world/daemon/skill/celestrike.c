58b35006ec64b9e728370ac5eea467b1777330b8
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:30:30 +0800

    Remove trailing spaces

--- world/daemon/skill/celestrike.c
+++ world/daemon/skill/celestrike.c
@@ -7,9 +7,9 @@ int effective_level() { return 10;}
 
 int valid_learn(object me)
 {
-        if( (int)me->query("bellicosity") < (int)me->query_skill("celestial") 
+        if( (int)me->query("bellicosity") < (int)me->query_skill("celestial")
 * 50 )
-                return 
+                return
 notify_fail("你的杀气不够，无法领悟更高深的天邪神功。\n");
 
         return 1;
@@ -17,7 +17,7 @@ notify_fail("你的杀气不够，无法领悟更高深的天邪神功。\n");
 
 int practice_skill(object me)
 {
-        return 
+        return
 notify_fail("天邪神功只能用学的，或是从运用(exert)中增加熟练度。\n");
 }
 
@@ -26,7 +26,7 @@ string exert_function_file(string func)
         return CLASS_D("fighter") + "/celestial/" + func;
 }
 
- 
+
 int learn_bonus()
 {
 	return 0;
