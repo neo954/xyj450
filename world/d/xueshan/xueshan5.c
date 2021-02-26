5eda982455088e5de0babebcb9c4bd9de25548d7
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:30:05 +0800

    Remove trailing spaces

--- world/d/xueshan/xueshan5.c
+++ world/d/xueshan/xueshan5.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 //standroom.c used by weiqi...others may hate this format:D
 //xueshan5.c
 
@@ -19,7 +19,7 @@ LONG);
    //set("item_desc", ([" *** " : " *** \n", ]));
    //for look something.
 
-   set("exits", 
+   set("exits",
    ([ //sizeof() == 4
      "south" : __DIR__"xueshan4",
      "north" : __DIR__"xueshan6",
@@ -28,8 +28,8 @@ LONG);
      //"up" : __DIR__"***",
      //"down" : __DIR__"***",
    ]));
-   
-   set("objects", 
+
+   set("objects",
    ([ //sizeof() == 1
      //__DIR__"npc/***" : 1,
    ]));
@@ -47,7 +47,7 @@ LONG);
 
 //int do_jump(string arg)
 //{
-       
+
 //}
 
 
