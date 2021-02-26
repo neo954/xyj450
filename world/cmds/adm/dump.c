7f0d2b7452f84138d5b0552559b631e2bf275cd8
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:19:28 +0800

    Remove trailing spaces

--- world/cmds/adm/dump.c
+++ world/cmds/adm/dump.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 // dump.c (should be admin cmd?)
 
 inherit F_CLEAN_UP;
@@ -18,12 +18,12 @@ int help(object me)
 {
    write(@HELP
 指令格式 : dump
- 
-dump the stats of all objects to a file in root dir 
+
+dump the stats of all objects to a file in root dir
 named OBJ_DUMP.
 
 HELP
     );
     return 1;
 }
- 
+
