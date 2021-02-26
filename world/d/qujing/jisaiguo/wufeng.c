21aa7546488e841172b215668f7e521a6dd55225
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:25:28 +0800

    Remove trailing spaces

--- world/d/qujing/jisaiguo/wufeng.c
+++ world/d/qujing/jisaiguo/wufeng.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 #include <room.h>
 inherit ROOM;
 
@@ -14,7 +14,7 @@ void create ()
 有桌椅，所有的客人都坐在地下。
 LONG);
 
-  set("objects", ([ /* sizeof() == 1*/ 
+  set("objects", ([ /* sizeof() == 1*/
   __DIR__"npc/king" : 1,
 ]));
   set("exits", ([ /* sizeof() == 2 */
