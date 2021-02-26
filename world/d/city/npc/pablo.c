43ef8bf2d72a3c638d9e335ce1928950cc7c9ea6
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:20:24 +0800

    Remove trailing spaces

--- world/d/city/npc/pablo.c
+++ world/d/city/npc/pablo.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 inherit NPC;
 
 void create()
@@ -9,10 +9,10 @@ void create()
    set("title", "当铺掌柜");
    set("gender", "男性");
    set("age", 47);
-   set("kee", 500); 
+   set("kee", 500);
    set("max_kee", 500);
    set("sen", 200);
-   set("max_sen", 200);   
+   set("max_sen", 200);
    set("combat_exp", 50000);
    set("attitude", "friendly");
    set("rank_info/respect", "掌柜的");
@@ -43,8 +43,8 @@ void accept_kill(object me)
                 ob=new("/d/city/npc/xunluobing");
                 ob->move(environment());
         }
-   message_vision("\n忽然从门外冲进来个巡逻官兵，对$N大喊一声“干什么？想杀人谋财么！\n\n",me);        
-   
+   message_vision("\n忽然从门外冲进来个巡逻官兵，对$N大喊一声“干什么？想杀人谋财么！\n\n",me);
+
         ob->kill_ob(me);
    ob->set_leader(me);
         me->fight_ob(ob);
