4b40c2bcbeebb4b62330a86b947a4a50272c9d91
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:30:00 +0800

    Remove trailing spaces

--- world/d/xueshan/boyroom.c
+++ world/d/xueshan/boyroom.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 //standroom.c used by weiqi...others may hate this format:D
 //girlroom.c
 
@@ -17,7 +17,7 @@ LONG);
    //set("item_desc", (["bed" : "一张精致的床铺(gosleep, gobed, bed)。\n", ]));
    //for look something.
 
-   set("exits", 
+   set("exits",
    ([ //sizeof() == 4
      "south" : __DIR__"restroom",
      //"north" : __DIR__"restroom",
@@ -26,8 +26,8 @@ LONG);
      //"up" : __DIR__"***",
      //"down" : __DIR__"***",
    ]));
-   
-   set("objects", 
+
+   set("objects",
    ([ //sizeof() == 1
      //__DIR__"npc/xunshi" : 2,
      //__DIR__"npc/yingwu-guai" : 1,
