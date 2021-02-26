01739d3c31799c219bd552e713de5aaffd6b869e
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:27:06 +0800

    Remove trailing spaces

--- world/d/qujing/wuzhuang/chengyun.c
+++ world/d/qujing/wuzhuang/chengyun.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 inherit ROOM;
 
 void create ()
@@ -13,12 +13,12 @@ void create ()
 村房楼舍，山上奇石怪松历历在目。
 LONG);
 
-   set("exits", 
+   set("exits",
    ([ //sizeof() == 4
      "down": __DIR__"yanzhen",
    ]));
-   
-   set("objects", 
+
+   set("objects",
    ([ //sizeof() == 1
      //__DIR__"npc/qingfeng" : 1,
      __DIR__"npc/zhenyuan" : 1,
