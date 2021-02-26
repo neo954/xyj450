731530819615ce3c4be56f34d833261a834cf74e
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:27:10 +0800

    Remove trailing spaces

--- world/d/qujing/wuzhuang/qiezidi.c
+++ world/d/qujing/wuzhuang/qiezidi.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 //qiezidi.c
 
 inherit ROOM;
@@ -14,13 +14,13 @@ void create ()
 来种蔬菜瓜果。这里是茄子地。
 LONG);
 
-   set("exits", 
+   set("exits",
    ([ //sizeof() == 4
      "southwest" : __DIR__"huangguadi",
      "north" : __DIR__"baicaidi",
    ]));
-   
-   set("objects", 
+
+   set("objects",
    ([ //sizeof() == 1
      //"/d/obj/food/dbc" : 2,
      //"/d/obj/food/xbc" : 2,
