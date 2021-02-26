4aa5679d8b4a783376bf1c1fd707419facb564be
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:21:02 +0800

    Remove trailing spaces

--- world/d/eastway/wangnan2.c
+++ world/d/eastway/wangnan2.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 inherit ROOM;
 
 void create ()
@@ -15,14 +15,14 @@ LONG);
 
         //set("item_desc", ([" *** " : " *** ", ]));
 
-        set("exits", 
+        set("exits",
         ([ //sizeof() == 4
                 "northeast" : __DIR__"wangnan1",
                 "south" : __DIR__"wangnan3",
      "west": __DIR__"jinshi",
         ]));
 
-        set("objects", 
+        set("objects",
         ([ //sizeof() == 1
 //                __DIR__"npc/libai" : 1,
         ]));
