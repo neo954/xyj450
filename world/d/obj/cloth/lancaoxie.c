f9029331a1037e65a6a55bc194b6f5fc4a8552f0
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:22:43 +0800

    Remove trailing spaces

--- world/d/obj/cloth/lancaoxie.c
+++ world/d/obj/cloth/lancaoxie.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 //lancaoxie.c
 #include <armor.h>
 
@@ -11,7 +11,7 @@ void create()
    set_name("烂草鞋", ({"lan caoxie", "caoxie", "shoes"}));
    set_weight(800);
    if( clonep() ) set_default_object(__FILE__);
-   else 
+   else
    {
      set("unit", "双");
      set("material", "cloth");
