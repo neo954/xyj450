f8f85d803d1fe7b29bb61085ea785ff1f0800865
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:23:30 +0800

    Remove trailing spaces

--- world/d/ourhome/obj/bishuizhou.c
+++ world/d/ourhome/obj/bishuizhou.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 // bishuizhou.c
 
 inherit ITEM;
@@ -15,7 +15,7 @@ void create()
                 set("unit", "张");
    set("long",
    "一张小纸片。上面写着“避水咒”三个字，不知道有什么用没有。\n");
-        set("material", "paper");       
+        set("material", "paper");
         }
 }
 
