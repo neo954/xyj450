8878f75446dd87c2d0aabc56bf79a492612947c1
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:27:08 +0800

    Remove trailing spaces

--- world/d/qujing/wuzhuang/mudanting.c
+++ world/d/qujing/wuzhuang/mudanting.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 //mudanting.c
 #include <room.h>
 inherit ROOM;
@@ -12,22 +12,22 @@ void create ()
 
       ***********************
       &      牡丹亭         &
-      ***********************         
+      ***********************
 
 果然是饮酒赏园的绝妙去处！北面桃林中甜香阵阵，大红蜜桃似
 乎伸手可及。南面的菊园中蜂飞鸟唱，时有几片金黄的菊瓣随风
 飘来。
 LONG);
 
-   set("exits", 
+   set("exits",
    ([ //sizeof() == 4
      "southwest" : __DIR__"juyuan",
      "north" : __DIR__"southpool",
      "down" : __DIR__"anshi",
      //"west" : __DIR__"westway6",
    ]));
-   
-   set("objects", 
+
+   set("objects",
    ([ //sizeof() == 1
      "/d/obj/misc/table" : 1,
      __DIR__"npc/hanzhongli" : 1,
