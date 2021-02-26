e0fc899dbfe846f421530f02ae57c16c411b2156
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:22:02 +0800

    Remove trailing spaces

--- world/d/meishan/feifeng.c
+++ world/d/meishan/feifeng.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 
 #include <room.h>
 inherit ROOM;
@@ -15,7 +15,7 @@ void create ()
 然有着幽雅的情趣。
 LONG);
 
-  set("objects", ([ /* sizeof() == 1*/ 
+  set("objects", ([ /* sizeof() == 1*/
   __DIR__"npc/erlang" : 1,
 ]));
   set("outdoors", 1);
