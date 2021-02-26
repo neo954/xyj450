5a0f19d0fa89d0de8f0862649d93eb624368befa
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:30:09 +0800

    Remove trailing spaces

--- world/daemon/class/fighter/doomforce/powerfade.c
+++ world/daemon/class/fighter/doomforce/powerfade.c
@@ -10,12 +10,12 @@ int exert(object me, object target, int amount)
 {
         int skill;
 
-        if( target != me ) return 
+        if( target != me ) return
 notify_fail("你只能用吹血神功压制自己的杀气。\n");
 
-        if( (int)me->query("force") < 100 )     return 
+        if( (int)me->query("force") < 100 )     return
 notify_fail("你的内力不够。\n");
-        if( me->query_temp("powerup") ) return 
+        if( me->query_temp("powerup") ) return
 notify_fail("你已经在运功中了。\n");
         if( (int)me->query("bellicosity") <= 0 )
                 return notify_fail("你现在毫无杀气。\n");
@@ -27,7 +27,7 @@ notify_fail("你已经在运功中了。\n");
         me->receive_damage("sen", 100);
 
         message_vision(
-                HIC "$N微一凝神，运起吹血神功，放慢呼吸，开始收敛自己的杀气 
+                HIC "$N微一凝神，运起吹血神功，放慢呼吸，开始收敛自己的杀气
 ....\n" NOR, me);
 
         if( me->is_fighting()
@@ -45,4 +45,4 @@ void remove_effect(object me, int amount)
         me->delete_temp("powerup");
         tell_object(me, "你的吹血神功运行完毕，将内力收回丹田。\n");
 }
- 
+
