82c99a11177543dd51d0222543471195619fae7f
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:20:17 +0800

    Remove trailing spaces

--- world/d/city/npc/bing.c
+++ world/d/city/npc/bing.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 inherit NPC;
 
 #include "greeting.h"
@@ -34,7 +34,7 @@ void init()
 {
         object ob;
         ::init();
-        if (interactive(ob = this_player()) && 
+        if (interactive(ob = this_player()) &&
                 (int)ob->query_condition("killer")) {
                 remove_call_out("kill_ob");
                 call_out("kill_ob", 1, ob);
