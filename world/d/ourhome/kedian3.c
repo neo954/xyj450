acca748b83dd944d6ad849d27bae646d23899fb7
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:23:29 +0800

    Remove trailing spaces

--- world/d/ourhome/kedian3.c
+++ world/d/ourhome/kedian3.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 // Room: /city/kedian3.c
 
 inherit ROOM;
@@ -10,7 +10,7 @@ void create ()
   set ("short", "荒郊野外");
   set ("long", @LONG
 你正走在荒郊野外的一条崎岖小路上，只有一些小虫子
-绕着你飞来飞去。 
+绕着你飞来飞去。
 LONG);
 
   set("outdoors", 1);
