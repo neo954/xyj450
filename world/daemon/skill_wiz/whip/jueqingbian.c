96dca2d293b1c6cacaafc38d7adccc2327fc50f3
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:31:06 +0800

    Remove trailing spaces

--- world/daemon/skill_wiz/whip/jueqingbian.c
+++ world/daemon/skill_wiz/whip/jueqingbian.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 // jueqing-bian.c
 // 绝情鞭法
 // Juney: 07/04/97: Genesis.
@@ -19,7 +19,7 @@ mapping *action =
 "damage_type" : "割伤",
 ]),
 ([
-"action" : 
+"action" :
 "$N默运冰心决，使出「几回寒暑」，一缕忽冷忽热的真气沿$w激射而来，径透$n丹田百脉",
 "dodge"  : -10,
 "damage" : 30,
@@ -29,7 +29,7 @@ mapping *action =
 "damage_type" : "内伤",
 ]),
 ([
-"action" : 
+"action" :
 "$N一式「君应有语」，神色凄楚惘然，$w斜斜舞出，$n心中一凛，只觉得这目光似曾相识，却又说不上来",
 "dodge"  : -5,
 "damage" : 20,
@@ -39,7 +39,7 @@ mapping *action =
 "damage_type" : "刺伤",
 ]),
 ([
-"action" : 
+"action" :
 "$N霞袖慢垂，莲步急趋，手中$w似影随形，一招「只影向谁」愈显落落欲往，矫矫不群",
 "dodge"  : -10,
 "damage" : 30,
@@ -49,7 +49,7 @@ mapping *action =
 "damage_type" : "擦伤",
 ]),
 ([
-"action" : 
+"action" :
 "$N徐徐舞动$w，却是一招「万里层云」，只见招断意连，缠绵婉转，一鞭未绝，便隐隐有万鞭相随",
 "dodge"  : 0,
 "damage" : 20,
@@ -59,7 +59,7 @@ mapping *action =
 "damage_type" : "割伤",
 ]),
 ([
-"action" : 
+"action" :
 "$N一式「千山暮雪」，$w挥洒自如，霎时间绛雪纷纷，娇如柳絮，弱似梨花，密密裹住$n全身"
 ,
 "dodge"  : -10,
@@ -70,7 +70,7 @@ mapping *action =
 "damage_type" : "刺伤",
 ]),
 ([
-"action" : 
+"action" :
 "只见$N一路「寂寞箫鼓」，$w回风舞雪，暗合韵理，虽无丝竹之音，反倒别有幽愁暗恨生，此时无声胜有声",
 "dodge"  : -10,
 "damage" : 20,
@@ -80,7 +80,7 @@ mapping *action =
 "damage_type" : "割伤",
 ]),
 ([
-"action" : 
+"action" :
 "$N一式「荒烟平楚」，手中$w吞吐不定，仿佛一层轻烟薄雾飘上$n肩头，若即若离，似真似幻，极尽悱恻哀艳之美",
 "dodge"  : -20,
 "damage" : 30,
@@ -90,7 +90,7 @@ mapping *action =
 "damage_type" : "割伤",
 ]),
 ([
-"action" : 
+"action" :
 "$N一声娇叱，$w上内劲贯注，矫夭飞舞，一路「莺燕黄土」所过之处落英缤纷，红消香断，劲风只刮得$n脸颊生痛",
 "dodge"  : 0,
 "damage" : 30,
@@ -100,7 +100,7 @@ mapping *action =
 "damage_type" : "内伤",
 ]),
 ([
-"action" : 
+"action" :
 "忽然间$N鞭势陡变，$w浏漓顿挫，古意盎然，一式「千秋万古」如月之曙，如气之秋，纵有千载情愁，亦化于这一招之中",
 "dodge"  : -10,
 "damage" : 20,
@@ -110,7 +110,7 @@ mapping *action =
 "damage_type" : "刺伤",
 ]),
 ([
-"action" : 
+"action" :
 "$N一式「情是何物」，鞭意缱绻缠绵，如泣如诉，犹似双丝网，中有千千结，直教$n剪之不断，理之还乱",
 "dodge"  : -10,
 "damage" : 20,
@@ -120,7 +120,7 @@ mapping *action =
 "damage_type" : "割伤",
 ]),
 ([
-"action" : 
+"action" :
 "$N袖舞时幻起花雨满天，鞭落处化作断霞千缕，一式「生死相许」鞭人合一，着着抢攻，全然不顾$n进招",
 "dodge"  : -20,
 "damage" : 30,
@@ -144,7 +144,7 @@ mapping *action_male =
 "damage_type" : "割伤",
 ]),
 ([
-"action" : 
+"action" :
 "$N默运冰心决，使出「几回寒暑」，一缕忽冷忽热的真气沿$w激射而来，径透$n丹田百脉",
 "dodge"  : 20,
 "damage" : 30,
@@ -153,7 +153,7 @@ mapping *action_male =
 "damage_type" : "内伤",
 ]),
 ([
-"action" : 
+"action" :
 "$N一式「君应有语」，神色凄楚惘然，$w斜斜挥出，$n心中一凛，只觉得这目光似曾相识，却�
 炙挡簧侠�",
 "dodge"  : 25,
@@ -164,7 +164,7 @@ mapping *action_male =
 "damage_type" : "刺伤",
 ]),
 ([
-"action" : 
+"action" :
 "$N云袖慢垂，莲步急趋，手中$w如影随形，一招「只影向谁」愈显落落欲往，矫矫不群",
 "dodge"  : 25,
 "damage" : 50,
@@ -174,7 +174,7 @@ mapping *action_male =
 "damage_type" : "擦伤",
 ]),
 ([
-"action" : 
+"action" :
 "$N徐徐舞动$w，却是一招「万里层云」，只见招断意连，缠绵婉转，一鞭未绝，便隐隐有万鞭�
 嗨�",
 "dodge"  : 30,
@@ -185,7 +185,7 @@ mapping *action_male =
 "damage_type" : "割伤",
 ]),
 ([
-"action" : 
+"action" :
 "$N一式「千山暮雪」，$w挥洒自如，霎时间绛雪纷纷，柔如柳絮，弱似梨花，密密裹住$n全身"
 ,
 "dodge"  : 30,
@@ -196,7 +196,7 @@ mapping *action_male =
 "damage_type" : "刺伤",
 ]),
 ([
-"action" : 
+"action" :
 "只见$N一路「寂寞箫鼓」，$w回风舞雪，暗合韵理，虽无丝竹之音，反倒别有幽愁暗恨生，此�
 蔽奚�び猩�",
 "dodge"  : 35,
@@ -207,7 +207,7 @@ mapping *action_male =
 "damage_type" : "割伤",
 ]),
 ([
-"action" : 
+"action" :
 "$N一式「荒烟平楚」，手中$w吞吐不定，仿佛一层轻烟薄雾飘上$n肩头，若即若离，似真似幻�
 ��°��а拗��",
 "dodge"  : 35,
@@ -218,7 +218,7 @@ mapping *action_male =
 "damage_type" : "割伤",
 ]),
 ([
-"action" : 
+"action" :
 "$N一声清啸，$w上内劲贯注，矫夭飞舞，一路「莺燕黄土」所过之处落英缤纷，红消香断，劲�
 缰还蔚�$n脸颊生痛",
 "dodge"  : 40,
@@ -229,7 +229,7 @@ mapping *action_male =
 "damage_type" : "内伤",
 ]),
 ([
-"action" : 
+"action" :
 "忽然间$N鞭势陡变，$w浏漓顿挫，古意盎然，一式「千秋万古」如月之曙，如气之秋，纵有千�
 厍槌睿�嗷�谡庖徽兄��",
 "dodge"  : 40,
@@ -240,7 +240,7 @@ mapping *action_male =
 "damage_type" : "刺伤",
 ]),
 ([
-"action" : 
+"action" :
 "$N一式「情是何物」，鞭意缱绻缠绵，如泣如诉，犹似双丝网，中有千千结，直教$n剪之不断�
 �碇�孤�",
 "dodge"  : 45,
@@ -251,7 +251,7 @@ mapping *action_male =
 "damage_type" : "割伤",
 ]),
 ([
-"action" : 
+"action" :
 "$N袖舞时幻起花雨满天，鞭落处化作断霞千缕，一式「生死相许」鞭人合一，着着抢攻，全然�
 还�$n进招",
 "dodge"  : 45,
@@ -266,7 +266,7 @@ mapping *action_male =
 mapping *action_sanshou =
 ({
 ([
-"action" : 
+"action" :
 "第一招「无孔不入」，乃是向对手全身各处大穴进攻，$n只觉得香风袭袭，周身百骸都是$w的�
 白�",
 "dodge"  : -30,
@@ -277,7 +277,7 @@ mapping *action_sanshou =
 "damage_type" : "刺伤",
 ]),
 ([
-"action" : 
+"action" :
 "紧跟着$N使出「无所不至」，虽为一招，其实千头万绪，$w同时点向$n周身诸处偏门穴道",
 "dodge"  : -30,
 "damage" : 90,
@@ -287,7 +287,7 @@ mapping *action_sanshou =
 "damage_type" : "刺伤",
 ]),
 ([
-"action" : 
+"action" :
 "第三招「无所不为」，不再点穴，而专击$n眼睛、咽喉、小腹、下阴等诸般柔软之处，阴毒无�
 �",
 "dodge"  : -30,
@@ -320,7 +320,7 @@ string query_skill_name(int level) {
 }
 */
 
-mapping query_action(object me, object weapon) 
+mapping query_action(object me, object weapon)
 {
 /*
     int level = (int)(me->query_skill("jueqing-bian", 1));
