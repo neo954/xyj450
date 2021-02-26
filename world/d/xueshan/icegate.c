e7b0094ab53b89cf64a7210e74d7d448359f1f7a
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:30:01 +0800

    Remove trailing spaces

--- world/d/xueshan/icegate.c
+++ world/d/xueshan/icegate.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 //standroom.c used by weiqi...others may hate this format:D
 //icegate.c
 
@@ -19,7 +19,7 @@ LONG);
    //set("item_desc", ([" *** " : " *** \n", ]));
    //for look something.
 
-   set("exits", 
+   set("exits",
    ([ //sizeof() == 4
      "south" : __DIR__"wuchang-n",
      //"north" : __DIR__"icegate",
@@ -28,8 +28,8 @@ LONG);
      //"up" : __DIR__"***",
      //"down" : __DIR__"***",
    ]));
-   
-   set("objects", 
+
+   set("objects",
    ([ //sizeof() == 1
      __DIR__"npc/tuying-zunzhe" : 1,
      __DIR__"npc/baixiang-zunzhe" : 1,
@@ -48,6 +48,6 @@ LONG);
 
 //int do_jump(string arg)
 //{
-       
+
 //}
 
