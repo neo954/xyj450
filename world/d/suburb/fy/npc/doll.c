d9c86395c36bde90aaffeb350f023187ea6ac19a
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:29:06 +0800

    Remove trailing spaces

--- world/d/suburb/fy/npc/doll.c
+++ world/d/suburb/fy/npc/doll.c
@@ -12,7 +12,7 @@ void create()
 	set("nickname", GRN"中场队员"NOR);
 	set("gender", "男性");
 	set("age", 20);
-	set("long", 
+	set("long",
 		"他就是著名的「阿甘」。金庸群侠传的自动启动程序就出自于他之手\n"
 		"他还是一位狂热的球迷，曾任TMN BBS足球板的板主。\n");
 	setup();
