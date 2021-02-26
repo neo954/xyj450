b217e1f399dab86351f332ae425200657a416d83
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:27:08 +0800

    Remove trailing spaces

--- world/d/qujing/wuzhuang/luobodi.c
+++ world/d/qujing/wuzhuang/luobodi.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 //luobodi.c
 
 inherit ROOM;
@@ -14,13 +14,13 @@ void create ()
 来种蔬菜瓜果。这里是萝卜地。
 LONG);
 
-   set("exits", 
+   set("exits",
    ([ //sizeof() == 4
      "north" : __DIR__"huludi",
      "southeast" : __DIR__"huangguadi",
    ]));
-   
-   set("objects", 
+
+   set("objects",
    ([ //sizeof() == 1
      "/d/obj/food/carrot" : 3,
    ]));
