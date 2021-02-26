6664c31efe436f89502c1b09895f4f2d14b579b1
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:29:35 +0800

    Remove trailing spaces

--- world/d/suburb/xkx/gaibang/inhole.c
+++ world/d/suburb/xkx/gaibang/inhole.c
@@ -12,7 +12,7 @@ void create()
 这是杨州城中央广场的老槐树内部，也是丐帮秘密的地下通道的总交
 汇处。树洞里丢满了各种拉圾，弭漫着一股臭哄哄的味道。树洞壁上钉着
 一块非常醒目的木牌（board）。
-LONG 
+LONG
 	);
 	set("exits", ([
 		"out" : "/d/suburb/xkx/city/guangchang"
@@ -40,7 +40,7 @@ int do_say(string arg)
 	if( !arg || arg=="" ) return 0;
 
 	if( arg=="天堂有路你不走呀"
-	||  arg=="地狱无门你进来啊") 
+	||  arg=="地狱无门你进来啊")
 	{
 		write("只听里面有人说了声：是本帮兄弟吧？请进来吧。\n");
 		message("vision", "树洞边上一堵墙缓缓移了开来，原来是道小木门。\n", this_player());
