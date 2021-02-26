c2ef1edd0d2720d01893a0887c86532bd12261c8
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:20:41 +0800

    Remove trailing spaces

--- world/d/city/zhongguan.c
+++ world/d/city/zhongguan.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 //changan city
 
 inherit ROOM;
@@ -17,13 +17,13 @@ LONG);
 
         //set("item_desc", ([" *** " : " *** ", ]));
 
-        set("exits", 
+        set("exits",
         ([ //sizeof() == 4
      "north" : __DIR__"baihu-w2",
      "south" : __DIR__"zhongguan2",
         ]));
 
-        set("objects", 
+        set("objects",
         ([ //sizeof() == 1
                 __DIR__"npc/guanjia" : 1,
                 "/d/obj/misc/tables" : 1,
