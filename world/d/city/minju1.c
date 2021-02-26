066bd49e1d223007ca669b0f41aa3857ccdf5012
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:20:16 +0800

    Remove trailing spaces

--- world/d/city/minju1.c
+++ world/d/city/minju1.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 //changan city
 
 inherit ROOM;
@@ -17,12 +17,12 @@ LONG);
 
         //set("item_desc", ([" *** " : " *** ", ]));
 
-        set("exits", 
+        set("exits",
         ([ //sizeof() == 4
      "west" : __DIR__"beiyin1",
         ]));
 
-        set("objects", 
+        set("objects",
         ([ //sizeof() == 1
                 __DIR__"npc/dog" : 1,
      __DIR__"npc/girl" : 1,
