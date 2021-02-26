1bf78871d254771d8520c6c68b80ebaf359b845e
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:29:09 +0800

    Remove trailing spaces

--- world/d/suburb/fy/npc/wind.c
+++ world/d/suburb/fy/npc/wind.c
@@ -9,7 +9,7 @@ void create()
 	set("nickname",HIC"云龙门开山祖师"NOR);
 	set("gender", "男性");
 	set("age", 20);
-	set("long", 
+	set("long",
 		"他就是当年的风，一剑平天下以后，功成身退，归隐山谷。\n"+
                 "他就是天神风云，北京城、神龙岛皆出自他手。\n");
 	
