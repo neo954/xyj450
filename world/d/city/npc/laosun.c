c29d77944a2d7e6236e265a3e532560ab9aecb06
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:20:23 +0800

    Remove trailing spaces

--- world/d/city/npc/laosun.c
+++ world/d/city/npc/laosun.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 // rewritten by snowcat on 5/23/1997
 
 inherit F_VENDOR_SALE;
@@ -60,7 +60,7 @@ void accept_kill(object me)
                 ob=new("/d/city/npc/xunluobing");
                 ob->move(environment());
         }
-        message_vision("\n忽然从门外冲进来个巡逻官兵，对$N大喊一声“干什么？想杀人谋财么！\n\n",me);        
+        message_vision("\n忽然从门外冲进来个巡逻官兵，对$N大喊一声“干什么？想杀人谋财么！\n\n",me);
 
         ob->kill_ob(me);
         ob->set_leader(me);
