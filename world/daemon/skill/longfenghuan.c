00df35e4d9fb22d9b58e9f27fee19ff27c33d9d7
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:30:38 +0800

    Remove trailing spaces

--- world/daemon/skill/longfenghuan.c
+++ world/daemon/skill/longfenghuan.c
@@ -3,7 +3,7 @@
 inherit SKILL;
 
 mapping *action = ({
-        ([      "action":               
+        ([      "action":
 "$N单脚金鸡独立，缓缓展出龙凤双环的起手式「八面威风」，手中$w挥向$n的丹田！",
                 "force":               50,
                 "dodge":                40,
@@ -11,7 +11,7 @@ mapping *action = ({
                 "post_action":  (: call_other, WEAPON_D, "bash_weapon" :),
                 "damage_type":  "挫伤"
         ]),
-        ([      "action":               
+        ([      "action":
 "$N手中$w相碰，发出＂隆隆＂声，一招「先声夺人」攻向$n$l！",
                 "force":               60,
                 "dodge":                20,
@@ -19,7 +19,7 @@ mapping *action = ({
                 "post_action":  (: call_other, WEAPON_D, "bash_weapon" :),
                 "damage_type":  "挫伤"
         ]),
-        ([      "action":               
+        ([      "action":
 "$N手中$w凌空一挥，使出龙凤双环中的「龙飞凤舞」击向$n$l！",
                 "force":               50,
                 "dodge":                10,
@@ -27,7 +27,7 @@ mapping *action = ({
                 "post_action":  (: call_other, WEAPON_D, "bash_weapon" :),
                 "damage_type":  "挫伤"
         ]),
-        ([      "action":               
+        ([      "action":
 "$N使出一招「游龙在天」，$w夹杂着风声撞向$n$l！",
                 "force":               80,
                 "dodge":                10,
@@ -40,7 +40,7 @@ mapping *action = ({
 int valid_learn(object me)
 {
         if( (int)me->query("str") + (int)me->query("max_force") / 10 < 50 )
-                return 
+                return
 notify_fail("你的膂力还不够，也许该练一练内力来增强力量。\n");
 
         return 1;
@@ -62,17 +62,17 @@ int practice_skill(object me)
 
         if( !objectp(weapon = me->query_temp("weapon"))
         ||      (string)weapon->query("skill_type") != "hammer" )
-                return 
+                return
 notify_fail("你必须先找一双环或者是类似的武器，才能练龙凤双环。\n");
 
         if( (int)me->query("kee") < 60 )
-                return 
+                return
 notify_fail("你的体力不够练龙凤双环，还是先休息休息吧。\n");
         me->receive_damage("kee", 60);
         return 1;
 }
 int effective_level() { return 20;}
- 
+
 string *parry_msg = ({
         "结果$n用手中的$v格开了$N的$w。\n",
 });
