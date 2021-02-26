37080456c59dc1ef512cbfdbda6a56cde1a16868
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:28:02 +0800

    Remove trailing spaces

--- world/d/suburb/es/canyon/canyon2.c
+++ world/d/suburb/es/canyon/canyon2.c
@@ -4,7 +4,7 @@ inherit ROOM;
 void create()
 {
         set("short","隘口小路");
-	set("long",@LONG                                   
+	set("long",@LONG
 来到这□，脚下的黄土仍是厚厚的一层，可见得来往这条道路的人必
 不多，向北即是隘口尾，往南一直通达天驼关口，两侧依旧是荒冷的山壁
 ，但似乎更为严峻。
