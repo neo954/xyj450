4de34ecc687e0d676dc4714bd2f982cd2e1623c0
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:28:01 +0800

    Remove trailing spaces

--- world/d/suburb/es/canyon/camp3.c
+++ world/d/suburb/es/canyon/camp3.c
@@ -4,7 +4,7 @@ inherit ROOM;
 void create()
 {
         set("short","小兵营帐");
-	set("long",@LONG                                   
+	set("long",@LONG
 这是个由粗略的布料所搭成的小营帐，□面只有三二个小兵，其他的
 小兵，大概在另一个小兵营帐□赌博聊天吧，而东边则是小队长的营帐。
 LONG
