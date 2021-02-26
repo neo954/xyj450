fd28958acc4940c2e6c281ecf29f5cef50f628ab
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:27:51 +0800

    Remove trailing spaces

--- world/d/sea/obj/food.c
+++ world/d/sea/obj/food.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 inherit ITEM;
 inherit F_FOOD;
 string *first_name = ({ "猪头","牛头","羊头"});
@@ -9,7 +9,7 @@ void create()
 {
    string name;
         name = first_name[random(sizeof(first_name))];
-   
+
         set_name(name, ({ "head" }) );
         set_weight(80);
         if( clonep() )
