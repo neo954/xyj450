448874d105131533bfce8ef6904bd75371278281
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:30:27 +0800

    Remove trailing spaces

--- world/daemon/condition/jjf_poison.c
+++ world/daemon/condition/jjf_poison.c
@@ -1,9 +1,9 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 int update_condition(object me, int duration)
 {
-                                                
+
         me->receive_wound("sen", 10);
         me->receive_wound("kee", 10);
         me->set_temp("death_msg","瘟病发作死了。\n");
@@ -13,7 +13,7 @@ int update_condition(object me, int duration)
                 message_vision("$N浑身颤个不停，似乎随时都会倒下去。\n", me);
         else if( (int)me->query("kee") < (int)me->query("max_kee")/2 )
                 message_vision("$N面目浮肿，嘴中不断流出黄沫。\n", me);
-        else 
+        else
                 message_vision("$N脸色发黄，四肢颤个不停。\n", me);
 
         if( duration < 1 ) return 0;
