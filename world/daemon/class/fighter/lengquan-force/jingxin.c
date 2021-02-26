0e3cd8fe720998aa85f321434c1eb4c2e5ce8ed1
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:30:09 +0800

    Remove trailing spaces

--- world/daemon/class/fighter/lengquan-force/jingxin.c
+++ world/daemon/class/fighter/lengquan-force/jingxin.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 #include <ansi.h>
 
 inherit F_CLEAN_UP;
@@ -11,13 +11,13 @@ int exert(object me, object target)
 {
         int skill;
 
-        if( target != me ) 
+        if( target != me )
                 return
 notify_fail("你只能用静心诀提高自己的警觉。\n");
 
-        if( (int)me->query("force") < 200 )     
+        if( (int)me->query("force") < 200 )
                 return notify_fail("你的内力不够。\n");
-        if( (int)me->query_temp("powerup") ) 
+        if( (int)me->query_temp("powerup") )
                 return notify_fail("你已经在运功中了。\n");
 
         skill = me->query_skill("lengquan-force",1);
