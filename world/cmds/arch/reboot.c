4542ce7f0a55266a27a753fc6602c0de0d5777d3
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:19:29 +0800

    Remove trailing spaces

--- world/cmds/arch/reboot.c
+++ world/cmds/arch/reboot.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 // reboot.c
 
 #include <login.h>
@@ -14,7 +14,7 @@ int main(object me, string arg)
    object npc;
 
    if( me != this_player(1) ) return 0;
-   
+
    wiz_status = SECURITY_D->get_status(me);
    if( wiz_status != "(admin)" && wiz_status != "(arch)" )
      return notify_fail("只有 (arch) 以上的巫师才能重新启动" + MUD_NAME + "\n");
@@ -34,11 +34,11 @@ int help (object me)
 {
         write(@HELP
 指令格式: reboot
- 
+
 重新起动游戏, 系统会开始倒数计时, 十五分钟后重新起动。
- 
+
 HELP
 );
         return 1;
 }
- 
+
