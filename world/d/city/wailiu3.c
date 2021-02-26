e3aefe0d8d031529f4fccad3cc0167ef8a2fd7b7
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:20:40 +0800

    Remove trailing spaces

--- world/d/city/wailiu3.c
+++ world/d/city/wailiu3.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 //changan city
 
 inherit ROOM;
@@ -18,14 +18,14 @@ LONG);
 
         //set("item_desc", ([" *** " : " *** ", ]));
 
-        set("exits", 
+        set("exits",
         ([ //sizeof() == 4
      "southwest" : __DIR__"wailiu2",
 //                "north" : __DIR__"huashi",
 //                "east" : __DIR__"mofang",
         ]));
 
-        set("objects", 
+        set("objects",
         ([ //sizeof() == 1
 //                __DIR__"npc/libai" : 1,
         ]));
