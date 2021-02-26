c6da4e51aaa1aaab7720ce489e6e47b52f5b92c8
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:30:17 +0800

    Remove trailing spaces

--- world/daemon/class/oldstuff/taoist/earth-warp.c
+++ world/daemon/class/oldstuff/taoist/earth-warp.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 // earth-warp.c
 
 #include <ansi.h>
@@ -12,11 +12,11 @@ inherit F_CLEAN_UP;
 int scribe(object me, object sheet, string arg)
 {
    if( me->is_fighting() )   return notify_fail("你正在战斗中！\n");
-   
+
    if( (int)me->query("mana") < 90 ) return notify_fail("你的法力不够了！\n");
    if( (int)me->query("sen") < 10 ) return notify_fail("你的精神不够集中！\n");
 
-   sheet->set_name(YEL "阴界传送符" NOR, ({ "earth-warp sheet", "sheet"}) );   
+   sheet->set_name(YEL "阴界传送符" NOR, ({ "earth-warp sheet", "sheet"}) );
    sheet->set("attach_func", (: call_other, __FILE__, "do_warp", arg :) );
    me->add("mana", -90);
    me->receive_damage("sen", 10);
@@ -44,6 +44,6 @@ int do_warp(string target, object who)
      CYN "你只觉得身子不断下坠，一直下坠，一直下坠，一直下坠，一直下坠 .....\n\n"
      NOR   "不知道什么时候，你发现你其实一直都站在坚硬的地面上，可是四周的景物\n\n"
         "却不一样了....\n", this_player() );
-   
+
    return 1;
 }
