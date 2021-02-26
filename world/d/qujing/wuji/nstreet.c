374069ec9d48a7cc371fa52369c54d9bae544df9
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:27:01 +0800

    Remove trailing spaces

--- world/d/qujing/wuji/nstreet.c
+++ world/d/qujing/wuji/nstreet.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 // Room: /u/mimi/zhuziguo/northstreet.c  snowcat moved to /d/qujing/wuji
 inherit ROOM;
 
@@ -21,7 +21,7 @@ LONG);
   set("objects", ([
     __DIR__"npc/people" : 3,
   ]));
-  set("outdoors", __DIR__"");                   
+  set("outdoors", __DIR__"");
 
   setup();
 }
