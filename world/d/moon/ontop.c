f927352c9f0cf384bc9a9d1a65bb717b5b253c67
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:22:15 +0800

    Remove trailing spaces

--- world/d/moon/ontop.c
+++ world/d/moon/ontop.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 // Room: /moon/ontop.c
 //cglaem...12/13/96.
 
@@ -39,7 +39,7 @@ LONG);
 int valid_leave(object me, string dir)
 {
    object wugang;
-   
+
     if (me->query("gender") == "男性" && dir == "north" ) {
         if(objectp(wugang=present("wu gang", environment(me))) && living(wugang) )
             return notify_fail("桂花树后转出吴刚，伸出一把大板斧拦住你道：\n慢着...院子里住的全是女人，你跑进去想干什么？\n");
