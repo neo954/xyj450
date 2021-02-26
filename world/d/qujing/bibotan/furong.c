ca9125f7bb38c53f27433ff6a59def0b96b0f7e5
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:24:06 +0800

    Remove trailing spaces

--- world/d/qujing/bibotan/furong.c
+++ world/d/qujing/bibotan/furong.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 #include <room.h>
 inherit ROOM;
 
@@ -15,7 +15,7 @@ void create ()
 电，天动地摇的前兆。
 LONG);
 
-  set("objects", ([ /* sizeof() == 1*/ 
+  set("objects", ([ /* sizeof() == 1*/
   __DIR__"npc/table" : 1,
 ]));
 //  set("outdoors", 1);
