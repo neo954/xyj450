d14ed83f52b0fb78e83ae718b4cca2d1c2540fc8
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:21:09 +0800

    Remove trailing spaces

--- world/d/gao/npc/kid.c
+++ world/d/gao/npc/kid.c
@@ -1,12 +1,12 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 inherit NPC;
 
 void create()
 {
        set_name("小学童", ({ "xuetong" }));
-       
+
 set("long","小学童背不了书，正被教书先生罚站．\n");
        set("gender", "男性");
        set("age", 10);
