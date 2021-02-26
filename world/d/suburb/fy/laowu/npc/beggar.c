e7771644e3c3242e90d82948ed1640888cbeaee9
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:29:00 +0800

    Remove trailing spaces

--- world/d/suburb/fy/laowu/npc/beggar.c
+++ world/d/suburb/fy/laowu/npc/beggar.c
@@ -18,7 +18,7 @@ void create()
         set("chat_msg", ({
                 "乞丐说道：好心的大爷哪～ 赏我要饭的几文钱吧～\n",
                 "乞丐懒洋洋地打了个哈欠。\n",
-                
+
 "乞丐伸手捉住了身上的虱子，骂道：老子身上没几两肉，全叫你们给咬糜了。 \n",
                 (: random_move :)
         }) );
@@ -28,15 +28,15 @@ void create()
 int accept_object(object me, object obj)
 {
         command("smile");
-        command("say 多谢这位" + RANK_D->query_respect(me) + 
+        command("say 多谢这位" + RANK_D->query_respect(me) +
 "，您好心一定会有好报的！");
         return 1;
 }
 
 int accept_fight(object me)
 {
-        command("say " + RANK_D->query_respect(me) + 
+        command("say " + RANK_D->query_respect(me) +
 "饶命！小的这就离开！\n");
         return 0;
 }
- 
+
