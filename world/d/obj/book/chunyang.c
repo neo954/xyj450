51b491d702f100a4f3515c044553c33d5952fe62
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:22:30 +0800

    Remove trailing spaces

--- world/d/obj/book/chunyang.c
+++ world/d/obj/book/chunyang.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 //chunyang.c
 
 inherit ITEM;
@@ -9,9 +9,9 @@ void create()
 {
    set_name("【纯阳心得】", ({"chunyang xinde","xinde","book", "shu"}));
    set_weight(1000);
-   if( clonep() ) 
+   if( clonep() )
      set_default_object(__FILE__);
-   else 
+   else
    {
      set("unit", "本");
      set("long", "一本小册子。几个龙飞凤舞的大字【纯阳心得】下还有一行小字：吕洞宾著。\n");
