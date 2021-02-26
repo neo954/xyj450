3c15beb73c49c7beebc0e2eae8c7b8dbed314417
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:20:06 +0800

    Remove trailing spaces

--- world/d/changan/playerhomes/bed_nonn.c
+++ world/d/changan/playerhomes/bed_nonn.c
@@ -1,11 +1,11 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 // /d/changan/playerhomes/stdbed.c
 // this  is a standard bed for married couples.
-// wiz should modify /d/changan/playerhomes/h_playername  to 
+// wiz should modify /d/changan/playerhomes/h_playername  to
 // connect here.
-// IMPORTANT: should copy this to bed_playername, otherwise...  
+// IMPORTANT: should copy this to bed_playername, otherwise...
 
 inherit ROOM;
 
@@ -16,13 +16,13 @@ void create()
 
 LONG
         );
-        
+
    set("exits", ([
                 "out": "/d/changan/playerhomes/h_nonn",
             ]));
    set("sleep_room",1);
    set("if_bed",1);
-                                              
+
         setup();
 }
 
