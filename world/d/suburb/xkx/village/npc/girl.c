084c7880506f7c88eeee45bef797e78efc2c515b
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:29:46 +0800

    Remove trailing spaces

--- world/d/suburb/xkx/village/npc/girl.c
+++ world/d/suburb/xkx/village/npc/girl.c
@@ -7,7 +7,7 @@ void create()
 	set_name("女孩",({ "girl" }) );
         set("gender", "女性" );
         set("age", 12);
-        set("long", 
+        set("long",
 "这是个农家少女，虽然只有十二、三岁，身材已经开始发育。\n");
         set_temp("apply/defense", 5);
 	set("combat_exp", 70);
