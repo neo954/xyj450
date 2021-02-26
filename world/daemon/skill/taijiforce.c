8905a62bd518ff35f5a49d04f947486b2e2c68ee
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:30:43 +0800

    Remove trailing spaces

--- world/daemon/skill/taijiforce.c
+++ world/daemon/skill/taijiforce.c
@@ -8,7 +8,7 @@ int valid_learn(object me) { return 1; }
 
 int practice_skill(object me)
 {
-        return 
+        return
 notify_fail("太极心法只能用学的，或是从运用(exert)中增加熟练度。\n");
 }
 
@@ -16,7 +16,7 @@ string exert_function_file(string func)
 {
         return CLASS_D("wudang") + "/taijiforce/" + func;
 }
- 
+
 int effective_level() { return 13;}
 
 int learn_bonus()
