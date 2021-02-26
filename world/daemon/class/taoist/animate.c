8037d41d8a02443f8a4e372ff60c5bb83854a2f6
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:30:21 +0800

    Remove trailing spaces

--- world/daemon/class/taoist/animate.c
+++ world/daemon/class/taoist/animate.c
@@ -5,15 +5,15 @@ inherit F_CLEAN_UP;
 int cast(object me, object target)
 {
         if( me->is_fighting() ) return notify_fail("你正在战斗中！\n");
-        
-        if( !target || !target->is_corpse() ) return 
+
+        if( !target || !target->is_corpse() ) return
 notify_fail("你要驱动哪一具尸体？\n");
-        
-        if( (int)me->query("mana") < 50 ) return 
+
+        if( (int)me->query("mana") < 50 ) return
 notify_fail("你的法力不够了！\n");
 
         message_vision(
-                
+
 "$N对著地上的$n喃喃地念了几句咒语，$n抽搐了几下竟站了起来！\n", me, target);
 
         target->animate(me, (int)me->query_skill("spells") * 3 + 30);
@@ -23,4 +23,4 @@ notify_fail("你的法力不够了！\n");
 
         return 1;
 }
- 
+
