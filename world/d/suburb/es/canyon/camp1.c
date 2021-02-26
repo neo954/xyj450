b3c7df0a5be86069f8067f3dd40fbef210f3cb82
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:28:01 +0800

    Remove trailing spaces

--- world/d/suburb/es/canyon/camp1.c
+++ world/d/suburb/es/canyon/camp1.c
@@ -4,7 +4,7 @@ inherit ROOM;
 void create()
 {
         set("short","军营入口");
-	set("long",@LONG                                   
+	set("long",@LONG
 此处为黄石隘口驻军的入口，东方为军队饮食的地方，在门口的两侧
 有两位小兵正在值勤。南、北两侧为小兵的营帐。
 LONG
