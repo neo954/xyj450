ef44653f83339ec0072c9deb6a2724047ef7a54d
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:22:20 +0800

    Remove trailing spaces

--- world/d/nanhai/obj/jingu2.c
+++ world/d/nanhai/obj/jingu2.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 // jingu.c 紧箍咒
 
 #include <ansi.h>
@@ -18,7 +18,7 @@ void create()
                 set("unit", "篇");
         set("long",
         "金箍咒，又唤做“定心真言”。撞见神通广大的妖魔，只依所用的咒语念一念，管教他入我门来。\n");
-        set("material", "paper");       
+        set("material", "paper");
         }
    setup();
 }
@@ -27,7 +27,7 @@ void init()
 {
    if (this_player() -> query("family/family_name") != "南海普陀山" ) {
      remove_call_out("free_jingu");
-     call_out("free_jingu", 120, this_object(), this_player()); 
+     call_out("free_jingu", 120, this_object(), this_player());
    }
 
    ::init();
@@ -42,7 +42,7 @@ void free_jingu(object ob, object who)
         + "随着一声轻响，金箍咒已腾空而去，不留下一丝痕迹。\n\n"NOR);
      destruct(ob);
    }
-   else { 
+   else {
      call_out("free_jingu", 120, ob, who);
    }
    return;
