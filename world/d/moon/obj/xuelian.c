3d182b506f94684c59a6cf6b14a5b9d97aa3e33d
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:22:15 +0800

    Remove trailing spaces

--- world/d/moon/obj/xuelian.c
+++ world/d/moon/obj/xuelian.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 // xuelian.c 雪莲
 #include <ansi.h>
 inherit ITEM;
@@ -19,7 +19,7 @@ void create()
      set("unit", "朵");
      set("value", 300);
        }
-   
+
    setup();
 }
 
@@ -30,7 +30,7 @@ void init()
 
 
 int do_chi(string arg)
-{   
+{
         if( !this_object()->id(arg) ) return 0;
         if( this_player()->is_busy() )
                 return notify_fail("你上一个动作还没有完成。\n");
@@ -40,14 +40,14 @@ int do_chi(string arg)
    if ( (int)this_player()->query("max_atman")>0)
    {
          message_vision(RED "$N吃下一朵雪莲花，忽觉一阵反胃，又尽数吐了出来。\n" NOR, this_player());
-   } 
-   else 
-   { 
+   }
+   else
+   {
       if(this_player()->query("gender")=="男性")
      message_vision(GRN "$N吃下一朵雪莲花，看起来精神多了。\n" NOR, this_player());
       else
      message_vision(GRN " $N吃下一朵雪莲花，脸显酡红，倍增娇艳。\n" NOR, this_player());
-   }   
+   }
    destruct(this_object());
    return 1;
 }
