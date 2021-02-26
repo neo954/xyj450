08467b054ab29dc7fee64349894c5ad144552e44
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:29:09 +0800

    Remove trailing spaces

--- world/d/suburb/fy/npc/rose.c
+++ world/d/suburb/fy/npc/rose.c
@@ -9,9 +9,9 @@ set_name("顾横波",({"rose"}));
 	set("nickname", HIG "流" NOR+HIY "彩" NOR +HIG "红" NOR);
 	set("gender", "女性");
 	set("age", 20);
-	set("long", 
-"这就是星宿大魔女顾横波");  
-  
+	set("long",
+"这就是星宿大魔女顾横波");
+
 
 	setup();
 	
