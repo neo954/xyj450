12d5c525fad7c2067818a2c206e5e9a668a36d55
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:25:09 +0800

    Remove trailing spaces

--- world/d/qujing/jingjiling/bolin.c
+++ world/d/qujing/jingjiling/bolin.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 #include <room.h>
 inherit ROOM;
 
@@ -14,7 +14,7 @@ void create ()
 处不可一见。
 LONG);
 
-  set("objects", ([ /* sizeof() == 1*/ 
+  set("objects", ([ /* sizeof() == 1*/
   __DIR__"npc/bo" : 1,
 ]));
   set("outdoors", 1);
