e087fc9cae00d7510adc36af8581eeb1901afade
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:20:16 +0800

    Remove trailing spaces

--- world/d/city/minju4.c
+++ world/d/city/minju4.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 //changan city
 
 inherit ROOM;
@@ -13,16 +13,16 @@ void create ()
 这户人家在长安城里也算比较富裕的了。男主人常年经商在外，妻子
 在家主持家务。如同长安城大多数人家一样，屋里供着南海观音和关
 王爷的神像。
-LONG); 
+LONG);
 
-        //set("item_desc", ([" *** " : " *** ", ])); 
+        //set("item_desc", ([" *** " : " *** ", ]));
 
-        set("exits", 
+        set("exits",
         ([ //sizeof() == 4
      "north" : __DIR__"beiyin5",
         ]));
 
-        set("objects", 
+        set("objects",
         ([ //sizeof() == 1
      __DIR__"npc/woman" : 1,
      __DIR__"npc/boy" : 1,
