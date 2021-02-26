ce01945121d02624d755c9524e06d5ebbea42ac9
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:19:38 +0800

    Remove trailing spaces

--- world/cmds/std/give.c
+++ world/cmds/std/give.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 // give.c
 
 inherit F_CLEAN_UP;
@@ -18,7 +18,7 @@ int main(object me, string arg)
 
     if(this_player()->is_busy())
       return notify_fail("你正忙着呢．．．\n");
-           
+
    if(!arg) return notify_fail("你要给谁什么东西？\n");
 
    if( sscanf(arg, "%s to %s", item, target)==2
@@ -35,15 +35,15 @@ int main(object me, string arg)
 
 
    if(sscanf(item, "%d %s", amount, item)==2) {
-     if( !objectp(obj = present(item, me)) )   
+     if( !objectp(obj = present(item, me)) )
         return notify_fail("你身上没有这样东西。\n");
      if( no_give=obj->query("no_give") )
         return notify_fail( stringp(no_give) ? no_give : "这样东西不能随意丢弃。\n");
-     if( !obj->query_amount() )   
+     if( !obj->query_amount() )
         return notify_fail( obj->name() + "不能被分开给人。\n");
      if( amount < 1 )
         return notify_fail("东西的数量至少是一个。\n");
-     if( amount > obj->query_amount() ) 
+     if( amount > obj->query_amount() )
         return notify_fail("你没有那么多的" + obj->name() + "。\n");
      else if( amount == (int)obj->query_amount() )
         return do_give(me, obj, who);
@@ -56,7 +56,7 @@ int main(object me, string arg)
                obj->set_amount( (int)obj->query_amount() - amount );
         } else
            destruct(obj2);
-        return rev_val; 
+        return rev_val;
      }
    }
 
@@ -84,10 +84,10 @@ int do_give(object me, object obj, object who)
    mixed no_give;
     if(me->is_busy())
       return notify_fail("你正忙着呢．．．\n");
-    
+
     if(who->is_busy())
       return notify_fail("对方正忙着呢，没时间理你．．．\n");
-// no_accept should be in do_give Bula    
+// no_accept should be in do_give Bula
     if(who->query("env/no_accept"))
            return notify_fail("对方好象不愿意收下你的东西。\n");
 
@@ -105,7 +105,7 @@ int do_give(object me, object obj, object who)
         message_vision("$N由衷地向$n道谢。\n",who,me);
         destruct(obj);
         return 1;
-              } 
+              }
      if( !(is_accept=who->accept_object(me, obj)) )
         return notify_fail("对方不要你的东西。\n");
      if( is_accept == 2 )
@@ -139,7 +139,7 @@ int help(object me)
 write(@HELP
 指令格式 : give <物品名称> to <某人>
       或 : give <某人> <物品名称>
- 
+
 这个指令可以让你将某样物品给别人, 当然, 首先你要拥有这样物品.
 
 不接受时 : set no_accept <任意值>
