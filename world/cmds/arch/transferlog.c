76afa41fdfbecf076d41c459bb320c82dc797329
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:19:30 +0800

    Remove trailing spaces

--- world/cmds/arch/transferlog.c
+++ world/cmds/arch/transferlog.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 // bboard.c
 
 
@@ -16,7 +16,7 @@ int main(object me, string arg)
    status = wizhood(me);
    if( me!=this_player(1) || wiz_level(me) < wiz_level(status) )
      return notify_fail("你没有权力使用这个指令。\n");
-   
+
    if (!arg) return 0;
         if (sscanf(arg, "%s %s", srcstr, dststr)!=2) return 0;
 
@@ -24,20 +24,20 @@ int main(object me, string arg)
 
         seteuid(getuid());
    user = new(LOGIN_OB);
-   if( (string)user->set("id", srcstr) != srcstr ) 
+   if( (string)user->set("id", srcstr) != srcstr )
      return notify_fail("Failed setting user name.\n");
    if (! user->restore())   return notify_fail("没有这个玩家。\n");
    export_uid(user);
 
-   if( (string)user->set("id", dststr) != dststr ) 
+   if( (string)user->set("id", dststr) != dststr )
      return notify_fail("Failed setting user name.\n");
         if (!user->save()) return notify_fail("存档失败！\n");
    destruct(user);
 
-        tell_object(me, 
+        tell_object(me,
      "玩家 "+srcstr+" 数据成功转换至玩家 "+dststr+" ！\n");
    return 1;
-}   
+}
 
 int help(object me)
 {
