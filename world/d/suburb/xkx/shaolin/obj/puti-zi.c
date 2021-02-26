1c1b12581eff58f1e2e9ff66c30a33ad4d88bf9f
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:29:43 +0800

    Remove trailing spaces

--- world/d/suburb/xkx/shaolin/obj/puti-zi.c
+++ world/d/suburb/xkx/shaolin/obj/puti-zi.c
@@ -49,7 +49,7 @@ int do_eat(string arg)
 	{
 /*		improve = force_limit - neili_limit;
 
-		if ( improve < 100 ) { 
+		if ( improve < 100 ) {
 			add_skill = force_skill + (100 - improve)/5 + 1;
 			me->set_skill("force", add_skill );
 		}
