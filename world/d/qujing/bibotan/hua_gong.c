ea95f17158849347e4aa185938adf68901493c8d
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:24:06 +0800

    Remove trailing spaces

--- world/d/qujing/bibotan/hua_gong.c
+++ world/d/qujing/bibotan/hua_gong.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 //巽卦象征风
 //巽、西南
 
@@ -18,11 +18,11 @@ void create ()
 衬托之下显得非常雅致。
 LONG);
 
-//  set("objects", ([ /* sizeof() == 1*/ 
+//  set("objects", ([ /* sizeof() == 1*/
 //  __DIR__"npc/erlang" : 1,
 //]));
 //  set("outdoors", 1);
-  
+
   set("position", "巽");
   set("exits", ([ /* sizeof() == 2 */
   "southeast" : __DIR__"hua_qian",
@@ -37,7 +37,7 @@ void init()
 }
 
 int do_jump(string arg)
-{       
+{
         object me;
         me=this_player();
 
