8069f410d8374d05d3be3945a89aa19fed737f82
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:28:04 +0800

    Remove trailing spaces

--- world/d/suburb/es/choyin/npc/obj/book1.c
+++ world/d/suburb/es/choyin/npc/obj/book1.c
@@ -5,10 +5,10 @@ inherit ITEM;
 void create()
 {
          string *names = ({
-                "「笑傲江湖」", "「天龙八部」", 
+                "「笑傲江湖」", "「天龙八部」",
                 "「七剑下天山」", "「游侠郭解传」",
                 "「七种武器」", "「楚留香传奇之血海飘香」",
-                "「四大名捕」", "「洪武剑侠图」" });     
+                "「四大名捕」", "「洪武剑侠图」" });
         set_name( names[random(sizeof(names))], ({ "book" }));
         set_weight(600);
         set("unit", "本");
@@ -16,9 +16,9 @@ void create()
         set("value", 100);
         set("skill", ([
                 "name":                 "unarmed",
-                "exp_required": 100, 
-                "sen_cost":             20, 
+                "exp_required": 100,
+                "sen_cost":             20,
                 "difficulty":   30,
-                "max_skill":    80,  
+                "max_skill":    80,
         ]) );
-}    
+}
