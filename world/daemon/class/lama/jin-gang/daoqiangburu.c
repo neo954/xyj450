ee08f1eac365745af9fca283b3595df96d086137
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:30:13 +0800

    Remove trailing spaces

--- world/daemon/class/lama/jin-gang/daoqiangburu.c
+++ world/daemon/class/lama/jin-gang/daoqiangburu.c
@@ -6,11 +6,11 @@ void remove_effect(object me, int amount);
 int perform(object me, object target)
 {
         int skill;
-        if( target != me ) return 
+        if( target != me ) return
 	notify_fail("你只能将［刀枪不入］用在自己的身上。\n");
         if( (int)me->query("force") < 100 )     return
         notify_fail("你的内力不够。\n");
-        if( (int)me->query_temp("daoqian") ) return 
+        if( (int)me->query_temp("daoqian") ) return
 	notify_fail("你已经在施展［刀枪不入］了。\n");
 	skill = me->query_skill_eff_lvl("jin-gang");
 	if(skill < 100) return notify_fail("你的金刚不坏功太差了！\n");
@@ -20,7 +20,7 @@ int perform(object me, object target)
 "$N拍了拍胸脯，高叫着：＂刀枪不入！刀枪不入！刀枪不入！．．．＂\n" NOR, me);
         me->add_temp("apply/iron-cloth", skill);
         me->set_temp("daoqian", 1);
-        me->start_call_out( (: call_other, __FILE__, "remove_effect", me, 
+        me->start_call_out( (: call_other, __FILE__, "remove_effect", me,
 skill :), skill/5);
         if( me->is_fighting() ) me->start_busy(3);
         return 1;
@@ -32,4 +32,4 @@ void remove_effect(object me, int amount)
         me->delete_temp("daoqian");
         tell_object(me, "你的［刀枪不入］的功效散光了。\n");
 }
- 
+
