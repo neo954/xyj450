162a0d8d8b57134478648e4f1bdf9bf5c6b124c6
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:28:02 +0800

    Remove trailing spaces

--- world/d/suburb/es/canyon/canyon4.c
+++ world/d/suburb/es/canyon/canyon4.c
@@ -6,7 +6,7 @@ inherit ROOM;
 void create()
 {
         set("short","隘口中道");
-	set("long",@LONG                                   
+	set("long",@LONG
 来到这□，风势比其他地方大了许多，或许是因为东边为军营入口通
 风较为良好的原因，南方一十里路的地方就是京城关口－天驼关的入口，
 在西边的山壁似乎过于平滑，不太自然，或许这□有什么秘密也说不定。
@@ -53,7 +53,7 @@ int do_swear(string arg)
     	message_vision(HIW"～～～～～～～～～～～～～～～～～～～～\n"NOR,me);
     	message_vision(HIW"～～～～～～～～～～～～～～～～～～～～\n"NOR,me);
     	message_vision(HIW"～～～～～～～～～～～～～～～～～～～～\n"NOR,me);
-    
+
     	tell_room(__DIR__"blackmarket",me->name()+"被传送过来了。\n");
     	tell_room(environment(me),me->name()+"突然间消失了\n",me);
     	this_player()->move(__DIR__"blackmarket");
