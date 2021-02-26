0844a327c47f78815bfffb8fc9636888beef112d
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:27:35 +0800

    Remove trailing spaces

--- world/d/qujing/zhuzi/nei.c
+++ world/d/qujing/zhuzi/nei.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 // by snowcat 11/6/1997
 inherit ROOM;
 
@@ -14,7 +14,7 @@ void create()
 软心枕。
 
 LONG);
-  set("exits", ([ 
+  set("exits", ([
     "west" : __DIR__"neiyuan",
     "southwest" : __DIR__"housan",
   ]));
