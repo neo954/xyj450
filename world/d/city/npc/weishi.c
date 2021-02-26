673cf0541a6965df4a9aae9a0d247654c7e13a3c
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:20:27 +0800

    Remove trailing spaces

--- world/d/city/npc/weishi.c
+++ world/d/city/npc/weishi.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 #include <ansi.h>
 
 inherit NPC;
@@ -39,7 +39,7 @@ void init()
 {
         object ob;
         ::init();
-        if (interactive(ob = this_player()) && 
+        if (interactive(ob = this_player()) &&
                 (int)ob->query_condition("killer")) {
                 remove_call_out("kill_ob");
                 call_out("kill_ob", 1, ob);
