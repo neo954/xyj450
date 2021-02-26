ae87f1e83927367866aa84f8b4fbdf6c53c7de66
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:30:12 +0800

    Remove trailing spaces

--- world/daemon/class/gaibang/ma-jw.c
+++ world/daemon/class/gaibang/ma-jw.c
@@ -9,7 +9,7 @@ void create()
 	set("title", "丐帮七袋弟子");
 	set("gender", "男性");
 	set("age", 35);
-	set("long", 
+	set("long",
 		"这是位武艺精强，却沉默寡言的丐帮八袋弟子。\n");
 	set("attitude", "peaceful");
 	set("str", 23);
@@ -25,15 +25,15 @@ void create()
 	
 	set("combat_exp", 69000);
 	
-	set_skill("force", 75); 
-	set_skill("huntian-qigong", 72); 
-	set_skill("unarmed", 75); 
-	set_skill("xianglong-zhang", 75); 
-	set_skill("dodge", 68); 
-	set_skill("xiaoyaoyou", 70); 
-	set_skill("parry", 70); 
-	set_skill("staff", 75); 
-	set_skill("begging", 70); 
+	set_skill("force", 75);
+	set_skill("huntian-qigong", 72);
+	set_skill("unarmed", 75);
+	set_skill("xianglong-zhang", 75);
+	set_skill("dodge", 68);
+	set_skill("xiaoyaoyou", 70);
+	set_skill("parry", 70);
+	set_skill("staff", 75);
+	set_skill("begging", 70);
 	
 	map_skill("force", "huntian-qigong");
 	map_skill("unarmed", "xianglong-zhang");
