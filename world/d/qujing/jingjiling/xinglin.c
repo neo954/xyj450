5eef0fae1e5af1902fe7071a5b2fb83b08e7f4d1
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:25:12 +0800

    Remove trailing spaces

--- world/d/qujing/jingjiling/xinglin.c
+++ world/d/qujing/jingjiling/xinglin.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 #include <room.h>
 inherit ROOM;
 
@@ -14,7 +14,7 @@ void create ()
 如墨，而无一花瓣凋解。
 LONG);
 
-  set("objects", ([ /* sizeof() == 1*/ 
+  set("objects", ([ /* sizeof() == 1*/
   __DIR__"npc/xing" : 1,
 ]));
   set("outdoors", 1);
