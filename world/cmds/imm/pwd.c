c8a17f6855e5fe3444f0364a06ff9f89846705bc
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:19:33 +0800

    Remove trailing spaces

--- world/cmds/imm/pwd.c
+++ world/cmds/imm/pwd.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 // pwd.c
 
 inherit F_CLEAN_UP;
@@ -18,10 +18,10 @@ int help(object me)
 {
 write(@HELP
 指令格式 : pwd
- 
+
 这个指令会显示出你目前的工作目录 (cwd) 及工作档案 (cwf).
 'update' 不加参数时便会更新你的 cwf.
- 
+
 HELP
     );
     return 1;
