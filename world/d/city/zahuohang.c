5d00a8478a83868852e751170616c1e9fe65c23d
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:20:41 +0800

    Remove trailing spaces

--- world/d/city/zahuohang.c
+++ world/d/city/zahuohang.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 //changan city
 
 inherit ROOM;
@@ -17,12 +17,12 @@ LONG);
 
         //set("item_desc", ([" *** " : " *** ", ]));
 
-        set("exits", 
+        set("exits",
         ([ //sizeof() == 4
      "south" : __DIR__"qinglong-e1",
         ]));
 
-        set("objects", 
+        set("objects",
         ([ //sizeof() == 1
                 __DIR__"npc/huoji" : 3,
         ]));
