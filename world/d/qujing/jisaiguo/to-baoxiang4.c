d36714e312b78cb7aa1ba3fa7432ab4f6c65cd20
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:25:26 +0800

    Remove trailing spaces

--- world/d/qujing/jisaiguo/to-baoxiang4.c
+++ world/d/qujing/jisaiguo/to-baoxiang4.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 #include <room.h>
 inherit ROOM;
 
@@ -13,7 +13,7 @@ void create ()
 的叫着，时不时路边的草丛中窜出只野兔，惊起几只鸟雀。
 LONG);
 
-  set("objects", ([ /* sizeof() == 1*/ 
+  set("objects", ([ /* sizeof() == 1*/
   __DIR__"npc/lingxuzi" : 1,
 ]));
   set("outdoors", 1);
