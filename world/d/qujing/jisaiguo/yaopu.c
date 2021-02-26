0b52839822962a875a206c1449a5b89970ae6544
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:25:28 +0800

    Remove trailing spaces

--- world/d/qujing/jisaiguo/yaopu.c
+++ world/d/qujing/jisaiguo/yaopu.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 #include <room.h>
 inherit ROOM;
 
@@ -14,7 +14,7 @@ void create ()
 来看病抓药的人把门槛都踏平了。
 LONG);
 
-  set("objects", ([ /* sizeof() == 1*/ 
+  set("objects", ([ /* sizeof() == 1*/
   __DIR__"npc/doctor" : 1,
 ]));
   set("exits", ([ /* sizeof() == 2 */
