0edfac498f2ac94eaf7284afbad7fad526afe7f8
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:27:12 +0800

    Remove trailing spaces

--- world/d/qujing/wuzhuang/westway6.c
+++ world/d/qujing/wuzhuang/westway6.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 inherit ROOM;
 
 void create ()
@@ -13,13 +13,13 @@ void create ()
 雕着一些山水人物。东面通到后殿。
 LONG);
 
-   set("exits", 
+   set("exits",
    ([ //sizeof() == 4
      "south" : __DIR__"westway5",
      "east" : __DIR__"houdian",
    ]));
-   
-   set("objects", 
+
+   set("objects",
    ([ //sizeof() == 1
      //__DIR__"npc/qingfeng" : 1,
      //__DIR__"npc/mingyue" : 1,
