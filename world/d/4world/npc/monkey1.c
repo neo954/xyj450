4efb4cb86e1f859d5045438192dceab1c19cb7d4
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:19:58 +0800

    Remove trailing spaces

--- world/d/4world/npc/monkey1.c
+++ world/d/4world/npc/monkey1.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 // monkey1.c
 
 inherit NPC;
@@ -11,9 +11,9 @@ void create()
    set("race", "野兽");
    set("age", 6);
    set("long", "一只顽皮的小猴子。\n");
-   
+
    set("str", 30);
-   set("cor", 26);   
+   set("cor", 26);
    set("combat_exp",50);
             set_skill("dodge",20);
             set_skill("unarmed",10);
@@ -27,7 +27,7 @@ void create()
      "啪地一声！小猴子扔来一颗野果，正好砸中你的鼻子。\n",
      "小猴子对你吱吱吱地叫了几声。\n",
      "小猴子抓耳挠腮地，围着你跳来跳去。\n" }) );
-     
+
 //   set_temp("apply/attack", 20);
 //   set_temp("apply/defense", 50);
 //   set_temp("apply/armor", 3);
@@ -51,9 +51,9 @@ void init()
 
 int do_let(string arg)
 {
-        if( !this_player()->query("pending/haha") ) 
+        if( !this_player()->query("pending/haha") )
         return notify_fail("小猴子口中呜呜两声，好像不愿理你！\n");
-   
+
    if(!arg )
    return notify_fail("小猴子好象不懂你说什么！\n");
 
