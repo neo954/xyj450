a2dde3b3e394c6ce3f39d4fe8d4b11ce117965cf
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:29:31 +0800

    Remove trailing spaces

--- world/d/suburb/xkx/city/npc/wujiang.c
+++ world/d/suburb/xkx/city/npc/wujiang.c
@@ -25,7 +25,7 @@ void create()
 	set_temp("apply/armor", 50);
 	set_temp("apply/damage", 30);
 
-	set("force", 400); 
+	set("force", 400);
 	set("max_force", 400);
 	set("jiali", 10);
 
@@ -38,7 +38,7 @@ void init()
 {
 	object ob;
 	::init();
-	if (interactive(ob = this_player()) && 
+	if (interactive(ob = this_player()) &&
 		(int)ob->query_condition("killer")) {
 		remove_call_out("kill_ob");
 		call_out("kill_ob", 1, ob);
