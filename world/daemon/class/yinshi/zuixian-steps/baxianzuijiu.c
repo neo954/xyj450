2aab00b9d7c8a2bc50fb03e5372ac5fdd8e90845
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:30:25 +0800

    Remove trailing spaces

--- world/daemon/class/yinshi/zuixian-steps/baxianzuijiu.c
+++ world/daemon/class/yinshi/zuixian-steps/baxianzuijiu.c
@@ -6,13 +6,13 @@ void remove_effect(object me, int amount);
 int perform(object me, object target)
 {
         int skill;
-        if( target != me ) return 
+        if( target != me ) return
 	notify_fail("你只能将［八仙醉酒］用在自己的身上。\n");
 	if(!me->query_condition("drunk"))
 	return notify_fail("你现在好象一点醉意也没有耶！\n");
         if( (int)me->query("force") < 100 )     return
         notify_fail("你的内力不够。\n");
-        if( (int)me->query_temp("zuixian") ) return 
+        if( (int)me->query_temp("zuixian") ) return
 	notify_fail("你已经在施展［八仙醉酒］了。\n");
 	skill = me->query_skill_eff_lvl("zuixian-steps");
 	if(skill < 100) return notify_fail("你的醉仙望月步太差了！\n");
@@ -21,7 +21,7 @@ int perform(object me, object target)
 "$N左摇右摆，俨然是醉仙望月步中的［八仙醉酒］，整个身形摇摆不定！\n" NOR, me);
         me->add_temp("apply/dodge", skill/3);
         me->set_temp("zuixian", 1);
-        me->start_call_out( (: call_other, __FILE__, "remove_effect", me, 
+        me->start_call_out( (: call_other, __FILE__, "remove_effect", me,
 skill/3 :), skill/3);
         if( me->is_fighting() ) me->start_busy(3);
         return 1;
@@ -33,4 +33,4 @@ void remove_effect(object me, int amount)
         me->delete_temp("zuixian");
         tell_object(me, "你的［八仙醉酒］施展完毕，身行稳定了下来。\n");
 }
- 
+
