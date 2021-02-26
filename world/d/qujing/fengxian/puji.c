f0b1676aadee527fd9d8c7b6d689a2edb0dfa52d
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:24:52 +0800

    Remove trailing spaces

--- world/d/qujing/fengxian/puji.c
+++ world/d/qujing/fengxian/puji.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 // created by snowcat 12/2/1997
 
 inherit ROOM;
@@ -13,7 +13,7 @@ void create ()
 普济寺门口前挤满了面黄饥瘦的灾民，在等待寺里一天一次的
 开众施舍。寺里面有几位虔诚的客人在烧香，寺后传来一阵阵
 木鱼声。
-    
+
 LONG);
 
   set("exits", ([
