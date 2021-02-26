81d1048ae2de8f81053ab85a4afd6fd1e65b7f07
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:22:06 +0800

    Remove trailing spaces

--- world/d/meishan/zhaoyun.c
+++ world/d/meishan/zhaoyun.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 
 #include <room.h>
 inherit ROOM;
@@ -15,7 +15,7 @@ void create ()
 涌起，嫩绿一片。
 LONG);
 
-  set("objects", ([ /* sizeof() == 1*/ 
+  set("objects", ([ /* sizeof() == 1*/
   __DIR__"npc/yao" : 1,
   __DIR__"npc/li" : 1,
 ]));
