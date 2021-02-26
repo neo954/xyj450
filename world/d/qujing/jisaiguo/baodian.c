3c6f7dc2a3fdc15a3d7efc595ef705d385af3bb9
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:25:19 +0800

    Remove trailing spaces

--- world/d/qujing/jisaiguo/baodian.c
+++ world/d/qujing/jisaiguo/baodian.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 #include <room.h>
 inherit ROOM;
 
@@ -14,7 +14,7 @@ void create ()
 面落着厚厚的灰尘，后面有道小门，象是通向个要紧的地方。
 LONG);
 
-  set("objects", ([ /* sizeof() == 1*/ 
+  set("objects", ([ /* sizeof() == 1*/
   __DIR__"npc/kusu" : 1,
 ]));
   set("exits", ([ /* sizeof() == 2 */
@@ -32,19 +32,19 @@ void open_down()
         if( !(tower = find_object(__DIR__"tower1")) )
                 tower = load_object(__DIR__"tower1");
 
-        if( tower = find_object(__DIR__"tower1") ) 
+        if( tower = find_object(__DIR__"tower1") )
         {
                 set("exits/enter", __DIR__"tower1");
                 message("vision", "苦宿禅师掏出把钥匙，将门上的铜锁打开。\n", this_object() );
      message("vision", "小门被人从外面打开了。\n", tower );
         set("long", @LONG
-    
+
 宝殿正中的一尊大佛好象已很久没有上金了，身上的金衣大部分
 脱落，看起来很滑稽。周围的菩萨罗汉竟都拿红布包了起来，上
 面落着厚厚的灰尘，后面有道小门，门开着，里面黑洞洞的。
 LONG);
    }
-   
+
         call_out("close_down", 300);
 }
 
@@ -60,7 +60,7 @@ void close_down()
 
         delete("exits/enter");
         set("long", @LONG
-    
+
 宝殿正中的一尊大佛好象已很久没有上金了，身上的金衣大部分
 脱落，看起来很滑稽。周围的菩萨罗汉竟都拿红布包了起来，上
 面落着厚厚的灰尘，后面有道小门，门上一把铜锁，象是通向个
