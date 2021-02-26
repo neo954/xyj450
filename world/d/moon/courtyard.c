babb1987d4e3ec1226c6a16f466c0dac60d7fe0b
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:22:07 +0800

    Remove trailing spaces

--- world/d/moon/courtyard.c
+++ world/d/moon/courtyard.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 #include <ansi.h>
 #include <room.h>
 inherit ROOM;
@@ -20,20 +20,20 @@ LONG);
 
   set("exits", ([ /* sizeof() == 4 */
   "southeast" : __DIR__"junkroom.c",
-  "west" : __DIR__"huilang1.c", 
+  "west" : __DIR__"huilang1.c",
          ]));
 
-  set("objects", ([ 
+  set("objects", ([
       __DIR__"npc/fairy5.c" : 1,
-    "/d/obj/flower/rose.c":2, 
+    "/d/obj/flower/rose.c":2,
         ]));
    set("no_clean_up", 0);
    set("resource", ([ /* sizeof() == 1 */
      "water" : 1,
    ]));
   set("outdoors", 1);
-      
-   setup();     
+
+   setup();
 //   "obj/board/moon_b"->foo();
 }
 
