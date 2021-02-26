21af8c7de663f2888cb6e3b2f48b197628e3a072
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:24:13 +0800

    Remove trailing spaces

--- world/d/qujing/bibotan/shuangyu.c
+++ world/d/qujing/bibotan/shuangyu.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 #include <room.h>
 inherit ROOM;
 
@@ -14,12 +14,12 @@ void create ()
 霜雨轩”三个大字，字体隽秀，象是出自女子之手。
 LONG);
 
-  set("objects", ([ /* sizeof() == 1*/ 
+  set("objects", ([ /* sizeof() == 1*/
   __DIR__"npc/gongzhu" : 1,
   __DIR__"npc/fuma" : 1,
 ]));
 //  set("outdoors", 1);
-  
+
 
   set("exits", ([ /* sizeof() == 2 */
   "out" : __DIR__"yuetai2",
