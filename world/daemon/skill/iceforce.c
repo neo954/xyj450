5862896c0cf271f7023cbf398e0ffca392f3ba46
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:30:36 +0800

    Remove trailing spaces

--- world/daemon/skill/iceforce.c
+++ world/daemon/skill/iceforce.c
@@ -8,7 +8,7 @@ int valid_learn(object me) { return 1; }
 
 int practice_skill(object me)
 {
-        return 
+        return
 notify_fail("意寒功只能用学的，或是从运用(exert)中增加熟练度。\n");
 }
 
@@ -23,17 +23,17 @@ mixed hit_ob(object me, object victim, int damage_bonus, int factor)
 
         foo = ::hit_ob(me, victim, damage_bonus, factor);
         if( intp(foo) && (damage_bonus + foo > 0) ) {
-                if( random(me->query_skill("iceforce")) > (damage_bonus + 
+                if( random(me->query_skill("iceforce")) > (damage_bonus +
 foo) ) {
                         victim->receive_wound("kee", (damage_bonus + foo));
                         victim->apply_condition("iceshock", factor/3);
-                        return 
+                        return
 "$N的招式挟著一股阴寒无比的劲风使得$n不禁打了个寒噤。\n";
                 }
         }
         return foo;
 }
- 
+
 int effective_level() { return 7;}
 
 int learn_bonus()
