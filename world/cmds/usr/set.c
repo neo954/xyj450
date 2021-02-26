5fa77067e1cd85b4b8e31a2c6ef5f3119119e4a4
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:19:46 +0800

    Remove trailing spaces

--- world/cmds/usr/set.c
+++ world/cmds/usr/set.c
@@ -1,30 +1,30 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 // set.c
- 
+
 #define MAX_ENV_VARS   20
- 
+
 inherit F_CLEAN_UP;
- 
+
 int help();
- 
+
 int main(object me, string arg)
 {
     int i;
     string term, *terms, *wiz_only, *allowed;
     mixed data;
     mapping env;
- 
+
     wiz_only = ({"invisibility", "immortal"});
     allowed=({  "block_tell", "brief", "brief_message", "brief_all",
      "no_accept", "no_teach", "wimpy",
      "fengbo-cha", "destination"});
- 
+
    if( me != this_player(1) ) return 0;
- 
+
    env = me->query("env");
- 
+
    if( !arg || arg=="" ) {
      write("你目前设定的环境变数有：\n");
      if( !mapp(env) || !sizeof(env) )
@@ -36,12 +36,12 @@ int main(object me, string arg)
      }
      return 1;
    }
- 
+
    if( sscanf(arg, "%s %s", term, data)!=2 ) {
      term = arg;
      data = "YES";
    }
- 
+
    if( term && term!="" ) {
      if( mapp(env) && undefinedp(env[term]) && sizeof(env) >= MAX_ENV_VARS )
         return notify_fail("你设的环境变数太多了，请先用 unset 删掉几个吧。\n");
@@ -65,17 +65,17 @@ int main(object me, string arg)
    }
    return help();
 }
- 
+
 int help()
 {
    write(@TEXT
 指令格式：set <变数名> [<变数值>]
- 
+
 这个指令让你设定一些环境变数，不加参数时会显示你目前设定的环境变数，不指定
 变数值，则内定值为 "YES"。
- 
+
 取消变数设定请用 unset 指令。
- 
+
 至于有哪些环境变数可以设定，请见 help settings。
 TEXT
    );
