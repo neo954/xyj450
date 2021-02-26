5f473e9148345bf0d516e01c5cee74a33659b1b5
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:30:07 +0800

    Remove trailing spaces

--- world/daemon/class/dragon/seashentong/breathe.c
+++ world/daemon/class/dragon/seashentong/breathe.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 #include <ansi.h>
 
 inherit SSERVER;
@@ -58,7 +58,7 @@ int cast(object me, object target)
 
            if( random(ap + dp) > dp ) {
                    damage = (int)me->query("max_mana") / 10 + random((int)me->query("eff_sen") / 5);
-                   damage -= (int)target->query("max_mana") / 10 + random((int)target->query("eff_sen") / 5);      
+                   damage -= (int)target->query("max_mana") / 10 + random((int)target->query("eff_sen") / 5);
         damage += (int)me->query("mana_factor")-random((int)target->query("mana_factor"));
 //here we can see if 2 players are at same status, the attacker has higher chance.
                    if( damage > 0 ) {
@@ -71,7 +71,7 @@ int cast(object me, object target)
                            target->receive_wound("kee", damage/4, me);
                            me->improve_skill("seashentong", 1, 1);
                    } else {
-//here, cast failed and the target's mana_factor will be added to the previous 
+//here, cast failed and the target's mana_factor will be added to the previous
 //damage to hurt yourself:(...note damage<0.
           msg += HIC "结果火势却被$n法力避住，反而是法主引火上身！\n" NOR;
           damage -= (int)target->query("mana_factor");
@@ -81,18 +81,18 @@ int cast(object me, object target)
                            me->receive_damage("kee", -damage*3/5, target);
                            me->receive_wound("kee", -damage/4, target);
             }
-                } 
+                }
      else
                    msg += "但是被$n躲开了。\n";
 
            message_vision(msg, me, target);
            if( damage > 0 ) COMBAT_D->report_status(target);
            else if( damage < 0 ) COMBAT_D->report_status(me);
-//damage=0 corresponding to "但是被$n躲开了。\n"--no report.   
+//damage=0 corresponding to "但是被$n躲开了。\n"--no report.
 
    }else{
 
-                
+
            msg = HIC"$N后退几步，口中念了几句咒文，大叫一声：“水德神龙何在？”\n\n"NOR;
            msg += HIC"半空中呼地现出两条气势威猛的银角背苍龙，纵身一吐，两股奇寒无比的水柱冲着$n喷去！\n"NOR;
 //divid by 10;
@@ -104,14 +104,14 @@ int cast(object me, object target)
            if (factor < 10){
                    factor = 10;
            }
-        
+
            ap = me->query_skill("spells");
            ap = ( ap * ap * ap / (4 * 400) ) * (int)me->query("sen");
            ap += (int)me->query("combat_exp");
            ap = ap*factor/10;
            dp = target->query("combat_exp");
-                        
-                        
+
+
            if( random(ap + dp) > dp ) {
                    damage = (int)me->query("max_mana") / 10 + random((int)me->query("eff_sen") / 5);
                    damage -= (int)target->query("max_mana") / 10 + random((int)target->query("eff_sen") / 5);
@@ -119,7 +119,7 @@ int cast(object me, object target)
 //here we can see if 2 players are at same status, the attacker has higher chance.
                    if( damage > 0 ) {
                      msg += HIC "结果$n躲避不及，被水柱激个正着！\n" NOR;
-//finally damage also depends on enabled spells level.   
+//finally damage also depends on enabled spells level.
                      damage +=random((damage*(int)me->query_skill("spells"))/80);
                            target->receive_damage("sen", damage*4/5, me);
                            target->receive_wound("sen", damage/3, me);
@@ -140,11 +140,11 @@ int cast(object me, object target)
            else
                    msg += "但是被$n躲开了。\n";
 
-           message_vision(msg, me, target); 
+           message_vision(msg, me, target);
            if( damage > 0 ) COMBAT_D->report_status(target);
            else if( damage < 0 ) COMBAT_D->report_status(me);
 //damage=0 corresponding to "但是被$n躲开了。\n"--no report.
-        
+
    }
 
         if( !target->is_fighting(me) ) {
