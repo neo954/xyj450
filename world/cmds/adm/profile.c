d39a8b5b54b80663174bd57651aa1c7da326c5a9
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:19:28 +0800

    Remove trailing spaces

--- world/cmds/adm/profile.c
+++ world/cmds/adm/profile.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 // profile.c
 
 int main(object me, string arg)
@@ -16,11 +16,11 @@ int help (object me)
 {
         write(@HELP
 指令格式: profile
- 
+
 turns on command profiling, the result will be saved to /PROFILE
- 
+
 HELP
 );
         return 1;
 }
- 
+
