a56d738511723c75254daa27663bb7cfd8be3fc0
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:28:09 +0800

    Remove trailing spaces

--- world/d/suburb/es/chuenyu/tunnel4.c
+++ world/d/suburb/es/chuenyu/tunnel4.c
@@ -22,7 +22,7 @@ LONG
          "rock": "很沉重的石板，但你似乎可以推动它。\n",
          "石板":"很沉重的石板，但你似乎可以推动它。\n"
          ]) );
-         
+
          setup();
 }
 
@@ -36,7 +36,7 @@ void check_trigger()
         object room;
         if( (int)query("trigger")==5//13
         &&      !query("exits/up") ) {
-                message("vision", 
+                message("vision",
 "石板终于被推的斜立起来，露出一个向上的缝隙。\n",
                         this_object() );
                 set("exits/up", __DIR__"east_castle");
@@ -54,12 +54,12 @@ void check_trigger()
 void close_passage()
 {
         object room;
-        
+
         if( !query("exits/up") ) return;
-        message("vision", 
+        message("vision",
 "石板“轰”的一声又倒了下来，将向上的通道盖住了。\n", this_object());
         if( room = find_object(__DIR__"east_castle") ) {
-                message("vision", 
+                message("vision",
 "石板“轰”的一声又倒了下去，将向下的通道盖住了。\n",
                         room );
                 room->delete("exits/down");
@@ -71,13 +71,13 @@ void close_passage()
 int do_push(string arg)
 {
         string dir;
-        
-        if( !arg || arg=="" ) 
+
+        if( !arg || arg=="" )
         {
         write("你要推什么？\n");
         return 1;
         }
-        
+
         if( arg=="rock" || arg == "石板") {
         add("trigger", 1);
         write("你试著往上用力推动石板，石板变得越来越松动了....\n");
@@ -92,4 +92,4 @@ void reset()
         ::reset();
         delete("trigger");
 }
-                                                                                                                                                                                       
+
