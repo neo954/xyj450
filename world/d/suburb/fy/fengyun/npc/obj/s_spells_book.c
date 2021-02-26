353a777dcfd052bbbf6b0a41d89f30a76997d6fb
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:28:41 +0800

    Remove trailing spaces

--- world/d/suburb/fy/fengyun/npc/obj/s_spells_book.c
+++ world/d/suburb/fy/fengyun/npc/obj/s_spells_book.c
@@ -11,21 +11,21 @@ void create()
         else {
                 set("unit", "本");
                 set("long",
-                        
+
 "这是一本茅山道术的入门，通过这本书，你可以初步领略到茅山术的奥妙\n"
                 );
                 set("value", 12260);
                 set("material", "paper");
                 set("skill", ([
-                        "name":     "necromancy",    // name of the skill  
-                    "exp_required": 500, // minimum combat experience equired 
+                        "name":     "necromancy",    // name of the skill
+                    "exp_required": 500, // minimum combat experience equired
                         "sen_cost":  30, // gin cost every time study this
                       "difficulty":  10,//the base int to learn this skill
         // modify is gin_cost's (difficulty - int)*5%
                     "max_skill":  20 // the maximum level you can learn
-                                                                               
+
         // from this object.
                 ]) );
         }
 }
- 
+
