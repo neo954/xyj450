c1ecb8f9d12b43dc9d3fb623a6fc4f93fdf83520
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:30:36 +0800

    Remove trailing spaces

--- world/daemon/skill/jingang-staff.c
+++ world/daemon/skill/jingang-staff.c
@@ -3,28 +3,28 @@
 inherit SKILL;
 
 mapping *action = ({
-        ([      "action":               
+        ([      "action":
 "$N使一招「天光乍现」，手中$w扫向$n下盘！",
                 "damage":               40,
                 "dodge":                10,
                 "parry":                -40,
                 "damage_type":  "挫伤"
         ]),
-        ([      "action":               
+        ([      "action":
 "$N挥起$w，一招「雨後初晴」攻向$n$l！",
                 "damage":               40,
                 "dodge":                20,
                 "parry":                -60,
                 "damage_type":  "挫伤"
         ]),
-        ([      "action":               
+        ([      "action":
 "$N横过手中$w，使出「浮云写意」击向$n$l！",
                 "damage":               40,
                 "dodge":                10,
                 "parry":                -40,
                 "damage_type":  "挫伤"
         ]),
-        ([      "action":               
+        ([      "action":
 "$N使出一招「游龙穿云」，$w一挺撞向$n$l！",
                 "damage":               40,
                 "dodge":                10,
@@ -36,7 +36,7 @@ mapping *action = ({
 int valid_learn(object me)
 {
         if( (int)me->query("str") + (int)me->query("max_force") / 10 < 50 )
-                return 
+                return
 notify_fail("你的膂力还不够，也许该练一练内力来增强力量。\n");
 
         return 1;
@@ -58,13 +58,13 @@ int practice_skill(object me)
 
         if( !objectp(weapon = me->query_temp("weapon"))
         ||      (string)weapon->query("skill_type") != "staff" )
-                return 
+                return
 notify_fail("你必须先找一根木杖或者是类似的武器，才能练杖法。\n");
 
         if( (int)me->query("kee") < 60 )
-                return 
+                return
 notify_fail("你的体力不够练这门杖法，还是先休息休息吧。\n");
         me->receive_damage("kee", 60);
         return 1;
 }
- 
+
