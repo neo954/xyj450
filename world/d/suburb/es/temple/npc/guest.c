708adbd536dbda68f24dec2eea48cc25a40b3993
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:28:26 +0800

    Remove trailing spaces

--- world/d/suburb/es/temple/npc/guest.c
+++ world/d/suburb/es/temple/npc/guest.c
@@ -5,7 +5,7 @@ void create()
      set_name("进香客", ({"guest"}) );
      set("gender", "男性");
      set("age", 31);
-     set("long", 
+     set("long",
          "一个福福泰泰的中年人, 他正气喘嘘嘘地走著石梯道\n");
      set("combat_exp", 600);
      set("attitude", "friendly");
