f6620d260895785c55105397c46be95f35bf4516
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:29:30 +0800

    Remove trailing spaces

--- world/d/suburb/xkx/city/npc/bing.c
+++ world/d/suburb/xkx/city/npc/bing.c
@@ -48,7 +48,7 @@ void init()
 {
 	object ob;
 	::init();
-	if (interactive(ob = this_player()) && 
+	if (interactive(ob = this_player()) &&
 		(int)ob->query_condition("killer")) {
 		remove_call_out("kill_ob");
 		call_out("kill_ob", 1, ob);
