1624bcec45fdb20e1202caeedde0917e2a726dc5
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:30:14 +0800

    Remove trailing spaces

--- world/daemon/class/ninemoon/ninemoonforce/powerup.c
+++ world/daemon/class/ninemoon/ninemoonforce/powerup.c
@@ -10,12 +10,12 @@ int exert(object me, object target, int amount)
 {
         int skill;
 
-        if( target != me ) return 
+        if( target != me ) return
 notify_fail("你只能用九阴心经提升自己的战斗力。\n");
 
-        if( (int)me->query("force") < 100 )     return 
+        if( (int)me->query("force") < 100 )     return
 notify_fail("你的内力不够。\n");
-        if( (int)me->query_temp("powerup") ) return 
+        if( (int)me->query_temp("powerup") ) return
 notify_fail("你已经在运功中了。\n");
 
         skill = me->query_skill("force");
@@ -25,14 +25,14 @@ notify_fail("你已经在运功中了。\n");
         me->receive_damage("kee", 0);
 
         message_vision(
-                HIR 
+                HIR
 "$N眼观鼻，鼻观口，口观心，施展九阴心经，聚虚为实，全身透出阴寒无比的真气！\n" NOR, me);
 
         me->add_temp("apply/attack", skill/4);
         me->add_temp("apply/dodge", skill/4);
         me->set_temp("powerup", 1);
 
-        me->start_call_out( (: call_other, __FILE__, "remove_effect", me, 
+        me->start_call_out( (: call_other, __FILE__, "remove_effect", me,
 skill/4 :), skill);
 
         if( me->is_fighting() ) me->start_busy(4);
@@ -47,4 +47,4 @@ void remove_effect(object me, int amount)
         me->delete_temp("powerup");
         tell_object(me, "你的九阴心经运行完毕，将内力收回丹田。\n");
 }
- 
+
