3704e1ad9120a893cba5b841cf9e5f5529f989ab
GONG Jie <gongjie.jie@gmail.com>
2021-02-26 18:23:40 +0800

    Remove trailing spaces

--- world/d/pantao/palace7.c
+++ world/d/pantao/palace7.c
@@ -1,6 +1,6 @@
 // 神话世界·西游记·版本４．５０
 /* <SecCrypt CPL V3R05> */
- 
+
 // created by snowcat.c 5/23/1997
 // room: /d/pantao/palace7.c
 
@@ -23,8 +23,8 @@ LONG);
   set("exits", ([
         "north"      : __DIR__"palace6",
         "south"      : __DIR__"fengb",
-        "northwest"  : __DIR__"pan3c",  
-        "northeast"  : __DIR__"pan3d",  
+        "northwest"  : __DIR__"pan3c",
+        "northeast"  : __DIR__"pan3d",
       ]));
 
   set("objects", ([
